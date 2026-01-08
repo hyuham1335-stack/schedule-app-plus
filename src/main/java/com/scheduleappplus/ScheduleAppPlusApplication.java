9bda7a8d1afee51369a19efc7b3ad4e795cf166d
hyu1335 <hyuham1335@gmail.com>
2026-01-08 15:40:16 +0900

    feat: 프로젝트 초기 환경 설정

---
+++ src/main/java/com/scheduleappplus/ScheduleAppPlusApplication.java
@@ -0,0 +1,13 @@
+package com.scheduleappplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleAppPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleAppPlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scheduleappplus/ScheduleAppPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scheduleappplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleAppPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
